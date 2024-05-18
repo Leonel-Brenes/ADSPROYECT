c8e6c18017a4df12bffc0358f75b632b64a4441e
Leonel-Brenes <131913971+Leonel-Brenes@users.noreply.github.com>
2024-05-18 08:07:34 -0600

    Delete ProyectoADS directory

--- ProyectoADS/ADSProject01/Models/Carrera.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ADSProject01.Models
-{
-    public class Carrera
-    {
-        public int IdCarrera { get; set; }
-        public string Codigo { get; set; }
-        public string Nombre { get; set; }
-    }
-}
--- ProyectoADS/ADSProject01/Models/Materia.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ADSProject01.Models
-{
-    public class Materia
-    {
-        public int IdMateria { get; set; }
-        public string NombreMateria { get; set; }
-    }
-}
